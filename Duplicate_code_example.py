242c2c18cfa21e0c5a273b6881fada143c8e6b84
Ellis Mendoza <ellismendoza09@gmail.com>
2023-10-29 22:25:32 -0700

    Update Duplicate_code_example.py
    Fix similar block issue - 1

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -6,5 +6,3 @@ perimeter = calculate_perimeter(4, 2)
 volume = calculate_volume(6, 3, 2)
 surface_area = calculate_surface_area(6, 3, 2)
 
-# Uses functions for calculating area and perimeter at once
-area, perimeter = calculate_area_perimeter(4, 2)
