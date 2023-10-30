327b3b81e76bf81f2a63604410dcee44b81f448c
Ellis Mendoza <ellismendoza09@gmail.com>
2023-10-29 21:30:55 -0700

    Update Duplicate_example.py
    Changed the params.

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -1,10 +1,10 @@
 from common_functions import *
 
 # Uses functions for area, perimeter, volume, and surface_area
-area = calculate_area(4, 2)
-perimeter = calculate_perimeter(4, 2)
-volume = calculate_volume(6, 3, 2)
-surface_area = calculate_surface_area(6, 3, 2)
+area = calculate_area(6, 4)
+perimeter = calculate_perimeter(6, 4)
+volume = calculate_volume(8, 4, 2)
+surface_area = calculate_surface_area(8, 4, 2)
 
 # Uses functions for calculating area and perimeter at once
-area, perimeter = calculate_area_perimeter(4, 2)
+area, perimeter = calculate_area_perimeter(6, 4)
