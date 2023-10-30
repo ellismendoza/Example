5063b0a41432c9b7c476c089109e7a6b74ae7985
Ellis Mendoza <ellismendoza09@gmail.com>
2023-10-29 22:24:57 -0700

    Update Duplicate_example.py
    Fix to similar block issue - 1

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -1,10 +1,5 @@
 from common_functions import *
 
-# Uses functions for area, perimeter, volume, and surface_area
-area2 = calculate_area(6, 4)
-perimeter2 = calculate_perimeter(6, 4)
-volume2 = calculate_volume(8, 4, 2)
-surface_area2 = calculate_surface_area(8, 4, 2)
-
 # Uses functions for calculating area and perimeter at once
-area3, perimeter3 = calculate_area_perimeter(6, 4)
+area, perimeter = calculate_area_perimeter(4, 2)
+
